fe94d267f7d2149701981b0be68790cc34ddd478
michalusio <micha_i@interia.pl>
2021-08-26 17:56:44 +0200

    Changed the repository for npm publishing

--- src/checkers/checker.ts
+++
@@ -1,7 +0,0 @@
-import { ASTStatement } from '../language/interfaces';
-import { visitTable } from '../visitor';
-import { typeDeclarationCheckVisitor } from './type-declaration-check';
-
-export function check(root: ASTStatement): ASTStatement {
-  return visitTable(['object', 'let', 'function', 'parameter', 'property'], typeDeclarationCheckVisitor(), root, undefined);
-}
--- src/optimizers/optimizer.ts
+++
@@ -1,7 +0,0 @@
-import { ASTStatement } from '../language/interfaces';
-import { visit } from '../visitor';
-import { arithmeticVisitor } from './arithmetic-visitor';
-
-export function optimize(root: ASTStatement): ASTStatement {
-  return visit('arithmetic', arithmeticVisitor, root, undefined);
-}
